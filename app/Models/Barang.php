8ba7d01c9f4293bb10143fa4ecd5626fda312028
Archiruz <evazerblyre@gmail.com>
2025-05-02 10:48:00 +0700

    feat: add docblocks for mass assignable attributes and relationships in Barang and Mutasi models

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -7,6 +7,22 @@ use App\Models\Mutasi;
 
 class Barang extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'nama_barang',
+        'kode',
+        'kategori',
+        'lokasi'
+    ];
+
+    /**
+     * Get mutasis for the barang.
+     */
     public function mutasis()
     {
         return $this->hasMany(Mutasi::class);
--- app/Models/Mutasi.php
+++ app/Models/Mutasi.php
@@ -8,11 +8,17 @@ use App\Models\Barang;
 
 class Mutasi extends Model
 {
+    /**
+     * Get user that owns the mutasi.
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Get barang that owns the mutasi.
+     */
     public function barang()
     {
         return $this->belongsTo(Barang::class);
